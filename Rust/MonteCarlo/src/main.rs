8209ef47fd03080be335676756ca83d334f78386
Jon Warghed <jon.warghed@gmail.com>
2015-03-09 21:27:08 +0100

    Rust is working as well, had to rethink

--- Rust/MonteCarlo/src/main.rs
+++ Rust/MonteCarlo/src/main.rs
@@ -1,40 +1,33 @@
 extern crate rand;
 use rand::distributions::{IndependentSample, Range};
-/*
+use std::old_io::stdin;
+
 fn main() {
-   let between = Range::new(0f64, 1.);
-   let mut rng = rand::thread_rng();
-
-   let total = 1_000_000;
-   let mut in_circle = 0;
-
-   for _ in 0..total {
-       let a = between.ind_sample(&mut rng);
-       let b = between.ind_sample(&mut rng);
-       if a*a + b*b <= 1. {
-           in_circle += 1;
-       }
-   }
-   // prints something close to 3.14159...
-   println!("{}", 4. * (in_circle as f64) / (total as f64));
+    let total = 1_000_000;
+    let hits = Infinity{okay:5}
+    .take(total)
+    .filter(|shot| shot.hit)
+    .collect::<Vec<Shot>>().len();
+    println!("{}", hits);
+    println!("{}", ((hits as f64) * 4.0)/(total as f64));
 }
-*/
-fn main() {
-   let total = 1_000_000;
-   let shots = (0..total).map(fire).collect();
-}
+//Empty struct not allowed, but how can i otherwise make the iterator?
+struct Infinity {okay:u32}
+struct Shot {hit: bool}
 
-fn fire<T>(x:T) -> FireEvent {
-    let mut rng = rand::thread_rng();
-    let between = Range::new(0f64, 1.);
-    let a = between.ind_sample(&mut rng);
-    let b = between.ind_sample(&mut rng);
-    FireEvent {hit : a*a + b*b <= 1.}
+impl Iterator for Infinity {
+    type Item = Shot;
+    fn next(&mut self) -> Option<Shot> {
+        let mut rng = rand::thread_rng();
+        let between = Range::new(0f64, 1.);
+        let a = between.ind_sample(&mut rng);
+        let b = between.ind_sample(&mut rng);
+        Some(Shot{hit: a*a + b*b <= 1.})
+    }
 }
-
-struct FireEvent { hit: bool, }
 
 #[test]
 fn should_be_able_to_fire(){
+
     let result = fire();
 }
